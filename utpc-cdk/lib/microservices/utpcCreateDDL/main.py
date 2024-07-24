f526cbd6b91202badbc61b8e06b1206f4ba15d01
paul <prramos1@utpl.edu.ec>
2024-07-24 17:00:48 -0500

    ultimos cambios

--- utpc-cdk/lib/microservices/utpcCreateDDL/main.py
+++ utpc-cdk/lib/microservices/utpcCreateDDL/main.py
@@ -114,6 +114,7 @@ def lambda_handler(event, context):
     
     response_email = send_html_email(ENV_SES_EMAIL_FROM, 
                                "plrmsr@gmail.com", 
+                               "racorrea2@utpl.edu.ec",
                                email_subject)
     
     print(f"Email sent successfully: {response_email}") # print(response_email)
